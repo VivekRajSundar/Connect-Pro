73cd52fd2feb9636401c785475b6466b33d1bbce
vivekrajsundar <vivekrajsundar@gmail.com>
2023-04-29 11:16:15 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./components/login";
 
 function App() {
   return (
     <div className="App">
-      <h2>Let's build linkedin clone</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }

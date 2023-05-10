689c7f87d0aa8eb83107703660a5bf44c2c237f0
vivekrajsundar <vivekrajsundar@gmail.com>
2023-05-10 22:47:17 +0530

    Added login functionality

--- src/Reducers/userReducers.js
+++ src/Reducers/userReducers.js
@@ -1,3 +1,5 @@
+import { SET_USER } from "../actions/actionType";
+
 const INITIAL_STATE = {
   user: null,
 };
@@ -5,6 +7,11 @@ const INITIAL_STATE = {
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     default:
+    case SET_USER:
+      return {
+        ...state,
+        user: action.user,
+      };
       return state;
   }
 };
--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import { connect } from "react-redux";
 import { signInAPI } from "../actions";
+import { Navigate } from "react-router-dom";
 
 const Login = (props) => {
   return (
     <Container>
+      {props && <Navigate to="/home" />}
       <Nav>
         <a href="">
           <img src="/images/login-logo.svg" alt="logo" />
